40b99207257832d8beb9e8bbe48a3198a371ab7e
HengWeiBin <hengweibin1898@gmail.com>
2020-04-26 16:06:37 +0800

    更新：修改首頁排版 更新：加入字體 更新：統一每頁的header

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    /*let hotZone = document.createElement("div"); 
+    hotZone.id = "fullscreen-hotzone"
+    hotZone.style.position = "fixed"
+    hotZone.style.top = "0"
+    hotZone.style.left = "0"
+    hotZone.style.margin = "0"
+    hotZone.style.height= "100px"
+    hotZone.style.width = "100%"
+    hotZone.addEventListener("mouseover", function(event){
+        if($(".header").css("display") == "none"){
+            $(".header").slideDown();
+        }
+    });
+    hotZone.addEventListener("mouseout", function(event){
+        $(".header").mouseleave(function(){
+            $(".header").slideUp();
+        });
+    });
+    document.body.appendChild(hotZone);
+*/
+});
