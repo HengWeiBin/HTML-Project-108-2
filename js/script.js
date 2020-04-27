d524b862c9ea1780676e3541c48c29009fc4b04f
HengWeiBin <hengweibin1898@gmail.com>
2020-04-27 21:11:59 +0800

    bug fixed for float header

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
-    /*let hotZone = document.createElement("div"); 
+    slideHeader();
+    document.addEventListener("scroll", function(){
+        let test = document.getElementById("fullscreen-hotzone");
+        if(document.documentElement.scrollTop > 50){
+            $(".header").css("display", "none");
+            test.addEventListener("mouseover", headerSlideDown);
+            test.addEventListener("mouseout", headerSlideup);
+        }
+        else{
+            $(".header").css("display", "flex");
+            test.removeEventListener("mouseover", headerSlideDown);
+            test.removeEventListener("mouseout", headerSlideup);
+        };
+    });
+});
+
+function slideHeader(){
+    let hotZone = document.createElement("div"); 
     hotZone.id = "fullscreen-hotzone"
     hotZone.style.position = "fixed"
     hotZone.style.top = "0"
@@ -7,16 +24,17 @@ $(document).ready(function(){
     hotZone.style.margin = "0"
     hotZone.style.height= "100px"
     hotZone.style.width = "100%"
-    hotZone.addEventListener("mouseover", function(event){
-        if($(".header").css("display") == "none"){
-            $(".header").slideDown();
-        }
-    });
-    hotZone.addEventListener("mouseout", function(event){
-        $(".header").mouseleave(function(){
-            $(".header").slideUp();
-        });
-    });
     document.body.appendChild(hotZone);
-*/
-});
+};
+
+function headerSlideup(event){
+    $(".header").mouseleave(function(){
+        $(".header").slideUp();
+    });
+};
+
+function headerSlideDown(event){
+    if($(".header").css("display") == "none"){
+        $(".header").slideDown();
+    }
+};
