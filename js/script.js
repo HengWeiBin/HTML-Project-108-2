e29836d575ff27aba5ed5c2e61049b65e284e745
HengWeiBin <hengweibin1898@gmail.com>
2020-04-29 21:16:18 +0800

    更新：新增當前國家顯示， 幫助按鈕，國家會因地區而改變 更新：改進header動畫

--- js/script.js
+++ js/script.js
@@ -1,22 +1,37 @@
 $(document).ready(function(){
+    //header滑動效果
     slideHeader();
     document.addEventListener("scroll", function(){
         if(document.documentElement.scrollTop > 50){
-            $("header").css("display", "none");
+            $("header").slideUp();
             $("header").addClass("header");
         }
         else{
-            $("header").css("display", "flex");
+            $("header").slideDown();
             $("header").removeClass("header");
         };
     });
 
+    //糕點圖片變化效果
     fade($(".cheese_cake"), "../images/pastry_1.png", "../images/pastry_1-1.png");
     fade($(".chocolate_cake"), "../images/pastry_2.png", "../images/pastry_2-1.png");
     fade($(".orange_pie"), "../images/pastry_3.png", "../images/pastry_3-1.png");
     fade($(".cinnamon_roll"), "../images/pastry_4.png", "../images/pastry_4-1.png");
     fade($(".macaron"), "../images/pastry_5.png", "../images/pastry_5-1.png");
     fade($(".pancake"), "../images/pastry_6.png", "../images/pastry_6-1.png");
+
+    let requestUrl = "http://ip-api.com/json";
+    $.ajax({
+        url: requestUrl,
+        type: 'GET',
+        success: function(json){
+          console.log("My country is: " + json.country);
+          $(".location p").html(json.country);
+        },
+        error: function(err){
+          console.log("Request failed, error= " + err);
+        }
+      });
 });
 
 function slideHeader(){
