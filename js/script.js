bdd7bad5a93f6913fc27a984c72163bfa89971ec
HengWeiBin <hengweibin1898@gmail.com>
2020-04-29 18:47:40 +0800

    no message

--- js/script.js
+++ js/script.js
@@ -10,6 +10,13 @@ $(document).ready(function(){
             $("header").removeClass("header");
         };
     });
+
+    fade($(".cheese_cake"), "../images/pastry_1.png", "../images/pastry_1-1.png");
+    fade($(".chocalate_cake"), "../images/pastry_2.png", "../images/pastry_2-1.png");
+    fade($(".orange_pie"), "../images/pastry_3.png", "../images/pastry_3-1.png");
+    fade($(".cinnamon_roll"), "../images/pastry_4.png", "../images/pastry_4-1.png");
+    fade($(".macaron"), "../images/pastry_5.png", "../images/pastry_5-1.png");
+    fade($(".pancake"), "../images/pastry_6.png", "../images/pastry_6-1.png");
 });
 
 function slideHeader(){
@@ -36,4 +43,21 @@ function headerSlideDown(event){
     if($(".header").css("display") == "none"){
         $(".header").slideDown();
     }
+};
+
+function fade(element, pic_Origin, pic_Transform){
+    let url_Origin = "url(" + pic_Origin + ")";
+    let url_Transform = "url(" + pic_Transform + ")";
+    element.mouseenter(function(){
+        element.fadeOut(400, function(){
+            element.css("background-image", url_Transform);
+            element.fadeIn(400);
+        });
+    });
+    element.mouseleave(function(){
+        element.fadeOut(400, function(){
+            element.css("background-image", url_Origin);
+            element.fadeIn(400);
+        });
+    });
 };
