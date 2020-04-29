9da6a7c75a1d095be24a3f645600aa92dadc3275
audipm98 <t107590061@ntut.edu.tw>
2020-04-29 11:03:34 +0800

    using css make zoom effect

--- js/script.js
+++ js/script.js
@@ -12,6 +12,23 @@ $(document).ready(function(){
     });
 });
 
+/*
+$(document).ready(function(){
+    $('img').hover(function(){
+        $(this).css("cursor","pointer");
+        $(this).toggle({
+            effect: "scale",
+            percent: "90%"
+        },200);
+    }, function(){
+        $(this).toggle({
+            effect: "scale",
+            percent: "80%"
+        },200);
+    });
+});
+*/
+
 function slideHeader(){
     let hotZone = document.createElement("div"); 
     hotZone.id = "fullscreen-hotzone"
