ef17b3c60c61fa3a97d3ed609b115153af381df9
HengWeiBin <hengweibin1898@gmail.com>
2020-06-28 14:37:36 +0800

    try to fixed : webpage auto reload when user touch screen

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-$(document).ready(function(){
+let deviceWidth = document.documentElement.clientWidth;
+
+$(document).ready(function () {
     new WOW().init();
 
     //糕點圖片變化效果
@@ -10,78 +12,79 @@ $(document).ready(function(){
     fade($("#pancake"));
 
     //Country Code 顯示
-    $.get("https://ipinfo.io", function(response) {
+    $.get("https://ipinfo.io", function (response) {
         console.log(response.city, response.country);
         $(".location p").html(response.country);
-        },
-         "jsonp");
+    },
+        "jsonp");
 
     //hamburger button animation
     $('.first-button').on('click', function () {
 
         $('.animated-icon1').toggleClass('open');
-      });
-      $('.second-button').on('click', function () {
-    
-        $('.animated-icon2').toggleClass('open');
-      });
-      $('.third-button').on('click', function () {
-    
-        $('.animated-icon3').toggleClass('open');
-      });
+    });
+    $('.second-button').on('click', function () {
 
-      DrawFooterLine();
+        $('.animated-icon2').toggleClass('open');
+    });
+    $('.third-button').on('click', function () {
+
+        $('.animated-icon3').toggleClass('open');
+    });
+
+    DrawFooterLine();
 });
 
-function fade(element){
-    element.mouseenter(function(){
+function fade(element) {
+    element.mouseenter(function () {
         element.carousel("next");
     });
-    element.mouseleave(function(){
+    element.mouseleave(function () {
         element.carousel("prev");
     });
 };
 
-//window.onresize = DrawFooterLine;   //call function when user resize browser
-window.onresize = function(){location.reload();};   //refresh all when user resize browser
+if (deviceWidth < 768) {
+    window.onresize = DrawFooterLine;   //call function when user resize browser
+}
+else {
+    window.onresize = function () { location.reload(); };   //refresh all when user resize browser
+}
 
 //footer 畫綫
-function DrawFooterLine(event){
+function DrawFooterLine(event) {
     let canvas = document.getElementById('line');
     let ctx = canvas.getContext("2d");
-    let width = document.documentElement.clientWidth;
-    
+
     canvas.height = 150;
     canvas.width = 300;
 
-    if (width >= 768)
-    {   // draw vertical line in tab or computer webside
+    if (deviceWidth >= 768) {   // draw vertical line in tab or computer webside
         ctx.beginPath()
         ctx.moveTo(280, 20)
         ctx.lineTo(280, 130)
     }
-    else
-    {   //draw horizontal line in phone website
+    else {   //draw horizontal line in phone website
         canvas.height = 15;
         ctx.beginPath()
         ctx.moveTo(30, 7)
         ctx.lineTo(260, 7)
     }
-    ctx.strokeStyle="white"
+    ctx.strokeStyle = "white"
     ctx.stroke()
 
 };
 
 //Scroll to menu part when user click on arrow down (phone page)
-$("#home_arrow_down").click(function(){
+$("#home_arrow_down").click(function () {
     $('html, body').animate({
         scrollTop: $("#home_menu").offset().top - $('.navbar').height()
     }, 500);
 });
 
-$("#campaign_link").click(function(){
+$("#campaign_link").click(function () {
     //Redirect to home page if this link is clicked at another page
-    if (!/index.html/.test(window.location.href)){
+    if (!/index.html/.test(window.location.href)) {
         window.location.href = "index.html#Campaign";
     }
     //else scroll to Campaign section
